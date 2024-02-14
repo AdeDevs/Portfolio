d485cd64b9bf4eab76876f1157a6d97f1d16390e
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-02-14 20:48:57 +0100

    updt

--- src/sreens/navbar.jsx
+++ src/sreens/navbar.jsx
@@ -1,17 +1,44 @@
 import { NavLink } from "react-router-dom";
+import Github from "../assets/github.png";
+import LinkedIn from "../assets/linkedin.png";
+import Twitter from "../assets/twitter.png";
 
 function NavScreen() {
-    return (
-        <div>
-            <h1>I am the navigator</h1>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/works">Works</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="nav-container">
+      <nav className="nav-bar">
+        <h1>
+          <NavLink to="/">AdeDevs</NavLink>
+        </h1>
+
+        <ul>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/works">Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="socials">
+        <ul>
+          <li>
+            <img src={Github} alt="github" />
+          </li>
+          <li>
+            <img src={LinkedIn} alt="linkedin" />
+          </li>
+          <li>
+            <img src={Twitter} alt="twitter" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default NavScreen;
