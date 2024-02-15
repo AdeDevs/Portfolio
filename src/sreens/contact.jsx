d23e9923c6ab96682ca881f177cbd222a2647798
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-02-15 16:02:59 +0100

    updt

--- src/sreens/contact.jsx
+++ src/sreens/contact.jsx
@@ -10,10 +10,10 @@ function ContactMe() {
                     {/* <li><a href="https://bit.ly/3UJB7LY">Lagos, Nigeria</a></li> */}
                 </ul>
             </header>
-        <form action="https://formsubmit.co/your@email.com">
+        <form action="https://formsubmit.co/your@email.com" method="POST">
           <input type="email" name="email"  required placeholder="Your Email" />
           <input id="name" type="text" name="name" required placeholder="Your Name" />
-          <textarea name="message" placeholder="Your message"></textarea>
+          <textarea required name="message" placeholder="Your message"></textarea>
           <button type="submit">Send</button>
         </form>
       </main>
